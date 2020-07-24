7e035810ac8d9697ca0788ab788a5925809b356b
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:30:15 -0400

    added tuesday stateful comp with props

---
+++ src/components/Tuesday.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Tuesday extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+        }
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Tuesday;
