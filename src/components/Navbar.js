146ad104b0f858ff69ff0f1ed54fc77fdb73481d
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:35:46 -0400

    navbar bootstrap dropdown upd

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+        }
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar;
