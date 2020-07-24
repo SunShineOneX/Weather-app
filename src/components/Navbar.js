5c2f8ac73ee77175c893206c8f7f87501d036120
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:39:43 -0400

    added navbar with dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,30 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                
+                <nav class="navbar navbar-expand-lg" style="background-color: #ffa931">
+  <a class="navbar-brand" href="#">Forecast</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
             </div>
         )
     }
