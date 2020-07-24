f9842b6ebaa852dc371391bc1e04765eb8e6e4d3
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:31:41 -0400

    added thursday comp with state and props

---
+++ src/components/Thursday.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Thursday extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+        }
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Thursday;
