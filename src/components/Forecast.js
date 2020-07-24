07e4edab1e582b5a7ba0c0024d9446c9088232ee
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:29:22 -0400

    added monday stateful comp with props

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default class Forecast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+        }
+        
+    }
+    
     render() {
         return (
             <div>
---
+++ src/components/Monday.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Monday extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+        }
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Monday;
