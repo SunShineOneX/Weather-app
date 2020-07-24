0566059601e70ce20ddcea83921c451b43e60cd3
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 17:27:05 -0400

    Forecast stateful component

---
+++ src/components/Forecast.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Forecast extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Forecast;
