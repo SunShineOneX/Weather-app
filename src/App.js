5be3ad14f73cca17b397cbabe00c3bafbaa1304f
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 19:39:36 -0400

    api key and variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+const api_key = "ecdcfaface86216d574a68c8d7711d35";
+const hidden_api_key = process.env.api_key;
+const lat = -0.13;
+const lon = 51.51;
+let part = "current,minutely,hourly"
+let apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
+exclude=${part}&appid=${hidden_api_key}`
 
 function App() {
   return (
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import axios from "/axios";
+import hidden_api_key from "app";
+import { hidden_api_key } from 'a';
+const BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid="+{hidden_api_key};
 
-class Forecast extends Component {
+class Forecast extends Component 
     constructor(props) {
         super(props);
         this.state = {
             info: [],
         }
-        
+    
+
+    async getForecast() {
+        try {
+            const res = await axios.get(BASE_URL + {hidden_api_key})
+        }
     }
     
     render() {
