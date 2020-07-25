37b322e315b52bba83a13b7e81266a58fce618bd
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 21:00:29 -0400

    api calls finally working properly

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
-import axios from "/axios";
-import hidden_api_key from "app";
-import { hidden_api_key } from 'a';
-const BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid="+{hidden_api_key};
+import axios from "axios";
 
-class Forecast extends Component 
+const api_key = "ecdcfaface86216d574a68c8d7711d35";
+const hidden_api_key = "6873977fa93f67c8469b30670cf21868";
+const city = "ellijay";
+const country = "us";
+let part = "current,minutely,hourly"
+const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${hidden_api_key}`;
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.7490&lon=-84.3880&
+// exclude=hourly,minutely,daily&appid=ecdcfaface86216d574a68c8d7711d35
+
+// api.openweathermap.org/data/2.5/weather?q=ellijay,us&appid=6873977fa93f67c8469b30670cf21868
+
+class Forecast extends Component {
     constructor(props) {
         super(props);
         this.state = {
             info: [],
         }
-    
+    }
 
     async getForecast() {
         try {
-            const res = await axios.get(BASE_URL + {hidden_api_key})
+            const res = await axios.get(apiCall)
+            console.log(res.data);
+            this.setState({info: res.data});
+        } catch {
+            console.log("error!!!!");
         }
+    }
+ 
+    componentDidMount() {
+        this.getForecast();
     }
     
     render() {
         return (
             <div>
                 <h1>Hello Forecast!</h1>
+                <h2>{this.state.info.name}</h2>
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,6 @@ import Friday from './Friday';
 import Saturday from './Saturday';
 import Sunday from './Sunday';
 
-const api_key = "ecdcfaface86216d574a68c8d7711d35";
-const hidden_api_key = process.env.api_key;
-const lat = -0.13;
-const lon = 51.51;
-let part = "current,minutely,hourly"
-let apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-exclude=${part}&appid=${hidden_api_key}`
-
 
 class Navbar extends Component {
     constructor(props) {
