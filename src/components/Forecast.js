f4faab7af4fb36402ea8b33dadd1e54df1236994
Jeff Rock <SunShineOneX@gmail.com>
2020-07-25 21:09:22 -0400

    fully reactive navbar

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,85 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
 
 const api_key = "ecdcfaface86216d574a68c8d7711d35";
 const hidden_api_key = "6873977fa93f67c8469b30670cf21868";
 const city = "ellijay";
 const country = "us";
-let part = "current,minutely,hourly"
-const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${hidden_api_key}`;
+let date = new Date().toString().slice(0, 10);
+let part = "current,minutely,hourly";
+const apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=4.6948076&lon=-84.4821498&exclude=hourly,minutely,current&appid=${hidden_api_key}`;
 const iconRender = `http://openweathermap.org/img/wn/10d@2x.png`;
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
+// exclude=hourly,minutely,current&appid=6873977fa93f67c8469b30670cf21868
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.7490&lon=-84.3880&
 // exclude=hourly,minutely,daily&appid=ecdcfaface86216d574a68c8d7711d35
 
+// 15957 00000
+// 15957 86400
+//  MAIN LINK: https://api.openweathermap.org/data/2.5/onecall?lat=4.6948076&lon=-84.4821498&exclude=hourly,minutely,current&appid=6873977fa93f67c8469b30670cf21868
 // api.openweathermap.org/data/2.5/weather?q=ellijay,us&appid=6873977fa93f67c8469b30670cf21868
 
 class Forecast extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            info: [],
-            info2: [],
-            info3: [],
-            icon: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: [],
+      weather: [],
+      info3: [],
+      icon: [],
+    };
+  }
+  async getForecast() {
+    try {
+      const res = await axios.get(apiCall);
+      console.log(res.data);
+      console.log(res.data.weather);
+      console.log(res.data.main.temp);
+      // this.setState({info: res.data.weather});
+      // this.setState({weather: this.state.info[0].main});
+      this.setState({ info: res.data });
+      this.setState({ weather: this.state.info.weather[0] });
+      this.setState({ info3: this.state.info.main });
+      this.setState({ icon: this.state.weather.icon });
+      // console.log(this.state.weather);
+      console.log(res.data.weather);
+      // console.log(res.data.main.temp);
+    } catch {
+      console.log("error!!!!");
     }
-    async getForecast() {
-        try {
-            const res = await axios.get(apiCall)
-            console.log(res.data);
-            console.log(res.data.weather);
-            console.log(res.data.main.temp);
-            // this.setState({info: res.data.weather});
-            // this.setState({info2: this.state.info[0].main});
-            this.setState({info: res.data});
-            this.setState({info2: this.state.info.weather[0]});
-            this.setState({info3: this.state.info.main});
-            this.setState({icon: this.state.info2.icon})
-        // console.log(this.state.info2);
-        console.log(res.data.weather);
-        // console.log(res.data.main.temp);
-    
-        } catch {
-            console.log("error!!!!");
-        }
-    }
-    // console.log(info);
-    // console.log(info2);
+  }
+  // console.log(info);
+  // console.log(weather);
 
-    // changePropertyName(property) {
-    //     this.state.propertyName = property
-    // }
- 
-    componentDidMount() {
-        this.getForecast();
-    }
-    
-    createIconLink = (iconId) => {
-        return `http://openweathermap.org/img/wn/${iconId}@2x.png`
-    }
+  // changePropertyName(property) {
+  //     this.state.propertyName = property
+  // }
 
-    render() {
-        
-        console.log(this.state.info2);
-        return (
-            <div>
-                <h1>Hello Forecast!</h1>
-                {/* {this.changePropertyName("weather")} */}
-                {/* <h2>{this.state.info.name}</h2> */}
-                <h2>{this.state.info2.main}</h2>
-                <h2>{this.state.icon}</h2>
-                <img src={this.createIconLink(this.state.icon)} />
-                <h2>{this.state.info3.temp}</h2>
-                
-                
+  componentDidMount() {
+    this.getForecast();
+  }
 
-                
-                {/* <h2>{this.state.info}</h2> */}
-                {/* <h2>{this.state.info.weather}</h2> */}
+  createIconLink = (iconId) => {
+    return `http://openweathermap.org/img/wn/${iconId}@2x.png`;
+  };
+
+  getFahrenheit = () => {
+    let kelvin = this.state.info3.temp;
+    let fahrenheit = Math.floor((kelvin - 273.15) * 1.8 + 32);
+    return fahrenheit;
+  };
+
+  getCurrentDay = (num) => {
+    let unix_timestamp = num;
+    var date = new Date(unix_timestamp * 1000);
+    return date.toString().slice(0,10);
+  }
+
+  render() {
+    console.log(this.state.weather);
+
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2">
+            <div id="card" class="card">
+              <h1>Today</h1>
+              <h2>{date}</h2>
+              {/* {this.changePropertyName("weather")} */}
+              {/* <h2>{this.state.info.name}</h2> */}
+              <h2>{this.state.weather.main}</h2>
+              <img src={this.createIconLink(this.state.icon)} />
+              <h2></h2>
+              <h2>{this.getFahrenheit()}</h2>
             </div>
-        )
-    }
+          </div>
+          <div class="col-lg-2">
+            <div id="card" class="card">
+              <h1>Today</h1>
+              <h2>{this.getCurrentDay()}</h2>
+              {/* {this.changePropertyName("weather")} */}
+              {/* <h2>{this.state.info.name}</h2> */}
+              <h2>{this.state.weather.main}</h2>
+              <img src={this.createIconLink(this.state.icon)} />
+              <h2></h2>
+              <h2>{this.getFahrenheit()}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Forecast;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,49 @@ import Thursday from './Thursday';
 import Friday from './Friday';
 import Saturday from './Saturday';
 import Sunday from './Sunday';
-
+import axios from "axios";
+const hidden_api_key = "6873977fa93f67c8469b30670cf21868";
+const apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=4.6948076&lon=-84.4821498&exclude=hourly,minutely,current&appid=${hidden_api_key}`;
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             info: [],
-        }
-        
+            day1: [],
+            day2: [],
+            day3: [],
+            day4: [],
+            day5: [],
+            day6: [],
+            day7: [],
+        }     
+    }
+    
+    async getForecast() {
+      try {
+        const res = await axios.get(apiCall);
+        this.setState({ info: res.data });
+        this.setState({ day1: this.state.info.daily[0].dt});
+        this.setState({ day2: this.state.info.daily[1].dt});
+        this.setState({ day3: this.state.info.daily[2].dt});
+        this.setState({ day4: this.state.info.daily[3].dt});
+        this.setState({ day5: this.state.info.daily[4].dt});
+        this.setState({ day6: this.state.info.daily[5].dt});
+        this.setState({ day7: this.state.info.daily[6].dt});
+      } catch {
+        console.log("error!!!!");
+      }
+    }
+
+    componentDidMount() {
+      this.getForecast();
+    }
+
+    getCurrentDay = (num) => {
+      let unix_timestamp = num;
+      var date = new Date(unix_timestamp * 1000);
+      return date.toString().slice(0,10);
     }
     
     render() {
@@ -35,13 +69,13 @@ class Navbar extends Component {
           Dropdown link
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#"><Link to="/Monday">Monday</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Tuesday">Tuesay</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Wednesday">Wednesday</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Thursday">Thursday</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Friday">Friday</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Saturday">Saturday</Link></a>
-          <a class="dropdown-item" href="#"><Link to="/Sunday">Sunday</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Monday">{this.getCurrentDay(this.state.day1)} Today</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Tuesday">{this.getCurrentDay(this.state.day2)}</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Wednesday">{this.getCurrentDay(this.state.day3)}</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Thursday">{this.getCurrentDay(this.state.day4)}</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Friday">{this.getCurrentDay(this.state.day5)}</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Saturday">{this.getCurrentDay(this.state.day6)}</Link></a>
+          <a class="dropdown-item" href="#"><Link to="/Sunday">{this.getCurrentDay(this.state.day7)}</Link></a>
         </div>
       </li>
     </ul>
